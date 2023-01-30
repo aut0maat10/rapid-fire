5804e032487cf56b5659f5459027e39b8eddfed8
Robin <robin.pahlman@gmail.com>
2023-01-30 13:24:44 -0500

    add start screen and basic flow logic

--- src/components/RapidFire.vue
+++ src/components/RapidFire.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="rapid-fire">
+    <StartScreen v-if="!quizStarted" @quizStarted="startHandler" />
     <component
+      v-if="quizStarted"
       :key="quizDataStore.quizData[counterStore.count]"
       :is="quizDataStore.quizData[counterStore.count].type"
       :quizData="quizDataStore.quizData[counterStore.count]"
@@ -10,8 +12,9 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
 import StandardQuestion from "./StandardQuestion.vue";
+import StartScreen from "./StartScreen.vue";
 import { useQuizDataStore } from "@/stores/QuizData";
 import { isCoreComponent } from "@vue/compiler-core";
 import { useCounterStore } from "../stores/counter";
@@ -19,6 +22,10 @@ import { useAnswersStore } from "../stores/answers";
 
 export default defineComponent({
   setup() {
+    onMounted(() => {
+      quizStarted.value = false;
+    });
+    const quizStarted = ref(false);
     const quizDataStore = useQuizDataStore();
     const counterStore = useCounterStore();
     const answersStore = useAnswersStore();
@@ -26,16 +33,23 @@ export default defineComponent({
       answersStore.submitAnswer(submission);
       counterStore.increment();
     };
+    const startHandler = () => {
+      quizStarted.value = true;
+    };
     return {
       quizDataStore,
       counterStore,
       answersStore,
       submitHandler,
+      onMounted,
+      quizStarted,
+      startHandler,
     };
   },
   components: {
     StandardQuestion,
     isCoreComponent,
+    StartScreen,
   },
 });
 </script>
---
+++ src/components/StartScreen.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>Hello World</div>
+  <button @click="startQuiz">Start Quiz</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "StartScreen",
+  emits: ["quizStarted"],
+  setup(props, { emit }) {
+    const startQuiz = () => {
+      emit("quizStarted");
+    };
+
+    return { emit, startQuiz };
+  },
+});
+</script>
+
+<style scoped></style>
