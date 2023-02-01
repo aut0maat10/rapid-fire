861bb460322df84336e6b9688b84b5b36c12de05
Robin <robin.pahlman@gmail.com>
2023-02-01 16:25:20 -0500

    more styling for standardQuestion component

--- src/components/StandardQuestion.vue
+++ src/components/StandardQuestion.vue
@@ -65,11 +65,52 @@ export default defineComponent({
   display: flex;
   flex-direction: column;
 }
-
-.options-button input {
-  margin-right: 1rem;
+.options-button {
+  display: flex;
 }
+.options-button input {
+  /* margin-right: 1rem; */
+  font-size: 2rem;
+  font-weight: bold;
+  line-height: 1.1;
+  /* display: grid; */
+  /* grid-template-columns: 1em auto;
+  gap: 0.5em; */
+}
+/* .next-question-button:active {
+  background-color: #7fbeab;
+} */
 input[type="radio"] {
   flex-shrink: 0;
+  appearance: none;
+  /* background-color: #fff; */
+  margin-right: 1rem;
+  font: inherit;
+  color: currentColor;
+  width: 1.15em;
+  height: 1.15em;
+  border: 0.15em solid currentColor;
+  border-radius: 50%;
+  transform: translateY(0.2em);
+  display: grid;
+  place-content: center;
+}
+input[type="radio"]::before {
+  content: "";
+  width: 0.65em;
+  height: 0.65em;
+  border-radius: 50%;
+  transform: scale(0);
+  transition: 120ms transform ease-in-out;
+  box-shadow: inset -1em 1em #2de1c2;
+}
+
+input[type="radio"]:checked::before {
+  transform: scale(1);
+}
+
+input[type="radio"]:focus {
+  outline: max(2px, 0.15em) solid currentColor;
+  outline-offset: max(2px, 0.15em);
 }
 </style>
