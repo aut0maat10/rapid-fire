b297989ee95f5202131ea7c25ee266521e9506c5
Robin Pahlman <robinpahlman@robins-mbp.mynetworksettings.com>
2023-09-07 12:29:43 -0400

    add tailwind, bg img, layout edits

--- src/components/RapidFire.vue
+++ src/components/RapidFire.vue
@@ -65,4 +65,5 @@ export default defineComponent({
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
--- src/components/StartScreen.vue
+++ src/components/StartScreen.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>Take this quiz to get started!</div>
+  <main class="startscreen" :style="{backgroundImage: `url(${backgroundImg})`}">
+    <div>
+      <h2 class="text-4xl font-bold">Where should you travel next?</h2>
+    </div>
   <button @click="startQuiz" class="rf-button">Start Quiz</button>
+  </main> 
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import backgroundImg from '@/assets/img/startscreen-bg.jpg'
 
 export default defineComponent({
   name: "StartScreen",
@@ -14,9 +19,22 @@ export default defineComponent({
       emit("quizStarted");
     };
 
-    return { emit, startQuiz };
+    return { emit, startQuiz, backgroundImg };
   },
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+  .startscreen {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  main {
+    background-size: cover;
+    background-position: 50% 40%;
+    box-shadow: inset 0 0 0 2000px rgba(22, 21, 22, 0.3);
+  }
+</style>
