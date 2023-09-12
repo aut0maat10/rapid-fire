b22688c731e11b4f18d7477229f29cdc99fc98ad
Robin Pahlman <robin.pahlman@gmail.com>
2023-09-12 11:08:32 -0400

    fighting composition API + typescript

--- src/components/EmailPrompt.vue
+++ src/components/EmailPrompt.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="flex flex-col justify-center content-center" :style="{backgroundImage: `url(${backgroundImg})`}">
         <h2 class="text-5xl font-bold text-slate-100 my-8">Are you ready to see your results?</h2>
+        <h1>{{ userData.userName }}</h1>
         <form class="flex flex-col" action="">
             <label for="name">Please enter your name</label>
-            <input type="name" v-model="formData.userName" placeholder="Type here" class="input input-bordered w-full max-w-xs">
+            <input type="name" v-model="userData.userName" placeholder="Type here" class="input input-bordered w-full max-w-xs">
             <label for="email">Your best email</label>
-            <input type="email" v-model="formData.userEmail" placeholder="test@testing.org" class="input input-bordered w-full max-w-xs">
-            <button class="btn btn-secondary" type="submit">See your results &#8594;</button>
+            <input type="email" v-model="userData.userEmail" placeholder="test@testing.org" class="input input-bordered w-full max-w-xs">
+            <button class="btn btn-secondary" :class="{ [`btn-active`]: canSubmit }" type="submit">See your results &#8594;</button>
         </form>
         
 
@@ -15,33 +16,27 @@
 
 <script lang="ts">
 
-import { defineComponent, defineProps, ref } from 'vue'
+import { defineComponent, reactive, computed } from 'vue'
 import type { PropType } from 'vue'
-import type { Ref } from "vue";
+// import type { Ref } from "vue";
 import backgroundImg from '@/assets/img/startscreen-bg.jpg'
 
 interface FormData {
-  userName?: string;
-  userEmail?: string;
+  userName: string;
+  userEmail: string;
 }
 
 export default defineComponent({
     name: 'EmailPrompt',
-    props: defineProps({
-    formData: {
-        type: Object as () => PropType<FormData>,
-        required: true
-    }
-}),
-  
-    setup(props) {
-        let formData: Ref<FormData> = ref({});
-
+    
+    setup() {
+        const userData = reactive<FormData>({userName: '', userEmail: ''})
+        const canSubmit = computed(() => userData.userEmail.length > 0 && userData.userName.length > 0)
         const handleSubmit = () => {
             alert('submitted!')
         };
 
-        return { formData, handleSubmit, backgroundImg }
+        return {userData, handleSubmit, backgroundImg, canSubmit }
     }
 })
 </script>
